2f5150ef913f5090974a7c422b4e8ff1d20ac597
1ritika <67949869+1ritika@users.noreply.github.com>
2025-01-18 14:44:11 +0530

    Add files via upload

---
+++ champsim_script.sh
@@ -0,0 +1,141 @@
+#Q2a
+#bimodal
+./build_champsim.sh bimodal no no no next_line lru 1
+
+./run_champsim.sh bimodal-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh bimodal-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh bimodal-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh bimodal-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+
+
+#gshare
+./build_champsim.sh gshare no no no next_line lru 1
+
+./run_champsim.sh gshare-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh gshare-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh gshare-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh gshare-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+
+
+#perceptron
+./build_champsim.sh perceptron no no no next_line lru 1
+
+./run_champsim.sh perceptron-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh perceptron-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh perceptron-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh perceptron-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+
+
+
+
+#tage
+./build_champsim.sh tage no no no next_line lru 1
+
+./run_champsim.sh tage-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh tage-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh tage-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh tage-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+
+
+
+#q2b
+
+#tage1
+./build_champsim.sh tage1 no no no next_line lru 1
+
+
+./run_champsim.sh tage1-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh tage1-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh tage1-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh tage1-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+
+
+#tage2
+./build_champsim.sh tage2 no no no next_line lru 1
+
+
+./run_champsim.sh tage2-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh tage2-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh tage2-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh tage2-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+
+#tage3
+./build_champsim.sh tage3 no no no next_line lru 1
+
+./run_champsim.sh tage3-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh tage3-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh tage3-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh tage3-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+#tage4
+./build_champsim.sh tage4 no no no next_line lru 1
+
+./run_champsim.sh tage4-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh tage4-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh tage4-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh tage4-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+
+#tage5
+./build_champsim.sh tage5 no no no next_line lru 1
+
+./run_champsim.sh tage5-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh tage5-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh tage5-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh tage5-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+#Q2C
+#hybrid1
+./build_champsim.sh hybrid1 no no no next_line lru 1
+
+./run_champsim.sh hybrid1-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh hybrid1-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh hybrid1-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh hybrid1-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+#hybrid2
+./build_champsim.sh hybrid2 no no no next_line lru 1
+
+./run_champsim.sh hybrid2-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh hybrid2-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh hybrid2-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh hybrid2-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+
+#hybrid3
+./build_champsim.sh hybrid3 no no no next_line lru 1
+
+./run_champsim.sh hybrid3-no-no-no-next_line-lru-1core 200 500 classification_phase4_core1.trace.xz -c
+./run_champsim.sh hybrid3-no-no-no-next_line-lru-1core 200 500 streaming_phase5_core3.trace.xz -c
+./run_champsim.sh hybrid3-no-no-no-next_line-lru-1core 200 500 nutch_phase3_core3.trace.xz -c
+./run_champsim.sh hybrid3-no-no-no-next_line-lru-1core 200 500 605.mcf_s-994B.champsimtrace.xz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
